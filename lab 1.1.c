d249b8e63a6addf911144e01e0e47312205cb65d
AShalyuta <ShAlexNiko@gmail.com>
2019-12-15 00:59:04 +0300

    Create lab 1.1.c

---
+++ lab 1.1.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char const *argv[])
+{
+    int x,a;
+    printf("Введите переменную x\n");
+    scanf("%d",&x);
+    a=x*x*x*4;
+    printf("%d\n",a);
+    return 0;
+}
